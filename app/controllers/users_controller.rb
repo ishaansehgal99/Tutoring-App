c75534f754f8d9ef9b80ac792004c30218b1ecbf
ishaanforthewin <ishaanforthewin@gmail.com>
2016-02-23 00:30:52 +0000

    minor tweaks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  include SessionsHelper
+  
   def new
     @user = User.new
   end
@@ -6,6 +8,7 @@ class UsersController < ApplicationController
   def create
 @user = User.new(user_params)
     if @user.save 
+      log_in @user
       redirect_to root_url 
       flash[:success] = "You have successfully signed up."
 
